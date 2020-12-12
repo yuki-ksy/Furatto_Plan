af515302fe3af04339667d081bebbca851464d30
yuki-ksy <yuki.k.97sy@gmail.com>
2020-12-12 09:22:17 +0000

    テストデプロイ

--- db/migrate/20201212064346_create_genres.rb
+++ db/migrate/20201212064346_create_genres.rb
@@ -2,7 +2,6 @@ class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
       t.string :genre
-
       t.timestamps
     end
   end
--- db/migrate/20201212064440_create_images.rb
+++ db/migrate/20201212064440_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.integer :tourism_id
-      t.string :image_
+      t.string :image_id
 
       t.timestamps
     end
